cebae30d72db136449927632063bb994b8cb90ad
Sandeep Suthar <ss.suthar.004@gmail.com>
2025-05-15 19:04:42 +0530

    files added

--- src/model_evaluation.py
+++ src/model_evaluation.py
@@ -6,26 +6,38 @@ import pickle
 
 from sklearn.metrics import accuracy_score, f1_score, recall_score, precision_score
 
-test_data = pd.read_csv("./data/processed/test_processed.csv")
+def load_test_data(filepath: str) -> pd.DataFrame:
+    return pd.read_csv(filepath)
 
-X_test = test_data.drop(columns=['deposit'])
-y_test = test_data['deposit']
 
-pipe = pickle.load(open("model.pkl", "rb"))
+def load_model(model_path: str):
+    with open(model_path, "rb") as file:
+        return pickle.load(file)
 
-y_pred = pipe.predict(X_test)
 
-accuracy = accuracy_score(y_test, y_pred)
-#precision = precision_score(y_test, y_pred)
-#recall = recall_score(y_test,y_pred)
-#f1 = f1_score(y_test, y_pred)
+def evaluate_model(model, X_test, y_test) -> dict:
+    y_pred = model.predict(X_test)
+    accuracy = accuracy_score(y_test, y_pred)
+    return accuracy
 
-metrics = {
-    'accuracy':accuracy,
-    #'precision':precision,
-    #'recall':recall,
-    #'f1':f1
-}
+def save_metrics(metrics: dict, output_path: str):
+    with open(output_path, "w") as file:
+        json.dump(metrics, file, indent = 4)
 
-with open('metrics.json', 'w') as file:
-    json.dump(metrics, file, indent=4)
+def main():
+    test_data_path = "./data/processed/test_processed.csv" 
+    mode_path = "model.pkl"
+    metrics_path = "metrics.json"
+
+    test_data = load_test_data(test_data_path)
+    X_test = test_data.drop(columns=['deposit'])
+    y_test = test_data['deposit']
+
+    model = load_model(mode_path)
+    metrics = evaluate_model(model, X_test, y_test)
+    save_metrics(metrics, metrics_path)
+
+
+
+if __name__ == "__main__":
+    main()
